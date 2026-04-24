e4422574b9a8aae3286a23f3061dc1dc962e5aef
Roi <tekky077@gmail.com>
2026-04-24 03:13:04 +0100

    adjusted hero section:

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,7 +10,9 @@ type HeroProps = {
 
 function Hero({title, description, mainText, subText, image}: HeroProps) {
   return (
-        <section id='hero' className="min-h-screen flex flex-col-reverse lg:flex-row items-center justify-between gap-12 px-6 md:px-12 lg:px-20 py-16 bg-white">
+        <section 
+        id="hero" 
+        className="min-h-screen flex flex-col-reverse lg:flex-row items-center justify-between gap-12 px-6 md:px-12 lg:px-20 py-16 bg-white">
             <div className='max-w-2xl text-center lg:text-left'>
                <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold leading-tight text-white">{title}</h1>
                <p className="mt-6 text-lg md:text-xl leading-8 text-gray-600">{description}</p>
